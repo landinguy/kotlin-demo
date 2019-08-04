3aee0befb7c2152d1908c618abb5973161f2d56c
landinguy <1029573528@qq.com>
2019-08-05 00:14:56 +0800

    update

--- src/main/kotlin/com/landinguy/kotlinDemo/KotlinDemoApplication.kt
+++ src/main/kotlin/com/landinguy/kotlinDemo/KotlinDemoApplication.kt
@@ -1,11 +1,13 @@
 package com.landinguy.kotlinDemo
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@MapperScan(basePackages = ["com.landinguy.kotlinDemo.dao"])
 class KotlinDemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlinDemoApplication>(*args)
+    runApplication<KotlinDemoApplication>(*args)
 }
